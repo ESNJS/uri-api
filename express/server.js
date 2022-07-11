cb7c68b887ed4e47e2d30ec3d61c238a06f2bd05
Erik <esnetto@gmail.com>
2022-07-11 17:25:53 -0300

    netlify mod

--- express/server.js
+++ express/server.js
@@ -1,9 +1,12 @@
+'use strict';
 const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const Web3 = require('web3-eth');
 
-
-const app = express();
+const router = express.Router();
 
 const providerRPC = {
     '820': 'https://rpc.callisto.network/',
@@ -14,7 +17,6 @@ const providerRPC = {
 const getClassForTokenIDABI = {"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"getClassForTokenID","inputs":[{"type":"uint256","name":"_tokenID","internalType":"uint256"}]}
 const getClassPropertiesABI = {"type":"function","stateMutability":"view","outputs":[{"type":"string[]","name":"","internalType":"string[]"}],"name":"getClassProperties","inputs":[{"type":"uint256","name":"_classID","internalType":"uint256"}]}
 
-
 async function getClassProperties(chainID, contractAddress, tokenID) {
     let web3 = new Web3(providerRPC[chainID]);
     let contract = new web3.Contract([getClassForTokenIDABI, getClassPropertiesABI], contractAddress)
@@ -22,22 +24,20 @@ async function getClassProperties(chainID, contractAddress, tokenID) {
     return JSON.parse("["+await contract.methods.getClassProperties(classID).call()+"]")
 }
 
-app.use(
-    cors({
-        credentials: true,
-        origin: true
-    })
-);
-app.options('*', cors());
-
-app.get('/:chainID/:contractAddress/:tokenID', async (req, res) => {
-    //console.log(req.params.chainID)
-    //console.log(req.params.contractAddress)
-    //console.log(req.params.tokenID)
+router.get('/:chainID/:contractAddress/:tokenID', async (req, res) => {
     let apr = await getClassProperties(req.params.chainID, req.params.contractAddress, req.params.tokenID)
     res.send(apr)
 });
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('server running on port https://localhost:3000', '');
-});
+app.use(
+    cors({
+        //credentials: true,
+        origin: '*'
+    })
+);
+app.options('*', cors());
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+module.exports = app;
+module.exports.handler = serverless(app);

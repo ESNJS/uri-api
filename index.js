fe3d5122a0f711f6ca70ae052545630320c80612
Erik <esnetto@gmail.com>
2022-07-11 16:09:03 -0300

    parse as json

--- index.js
+++ index.js
@@ -14,12 +14,11 @@ const providerRPC = {
 const getClassForTokenIDABI = {"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"getClassForTokenID","inputs":[{"type":"uint256","name":"_tokenID","internalType":"uint256"}]}
 const getClassPropertiesABI = {"type":"function","stateMutability":"view","outputs":[{"type":"string[]","name":"","internalType":"string[]"}],"name":"getClassProperties","inputs":[{"type":"uint256","name":"_classID","internalType":"uint256"}]}
 
-
 async function getClassProperties(chainID, contractAddress, tokenID) {
     let web3 = new Web3(providerRPC[chainID]);
     let contract = new web3.Contract([getClassForTokenIDABI, getClassPropertiesABI], contractAddress)
     let classID = await contract.methods.getClassForTokenID(tokenID).call()
-    return await contract.methods.getClassProperties(classID).call()
+    return JSON.parse("["+await contract.methods.getClassProperties(classID).call()+"]")
 }
 
 app.use(
